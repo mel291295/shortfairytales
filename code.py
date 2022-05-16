4c8586efeee8b59a31658cf49d775060271fb9a7
mel291295 <102040009+mel291295@users.noreply.github.com>
2022-05-16 15:36:13 +0200

    Update code.py

--- code.py
+++ code.py
@@ -4,12 +4,16 @@ st.title("Once upon a time")
 st.subheader("This is a web app to summarize and translate Grimm's fairy tales")
 st.caption("You can choose your favorite fairy tale and the app will summerize it for you into a few lines in whatevere language you want.")
 
-filename = st.text_input('Enter a file path:', /content/fff_fulltext.txt)
-try:
-    with open(filename) as input:
-        st.text(input.read())
-except FileNotFoundError:
-    st.error('File not found.')
+#upload file cvs file 
+# app.py, run with 'streamlit run app.py'
+import pandas as pd
+import streamlite as st
+
+df = pd.read_csv("/content/grimms_fairytales (1).csv")  
+# df = pd.read_excel(...)  # will work for Excel files
+
+st.title("Fairytales")  # add a title
+st.write(df)  # visualize my dataframe in the Streamlit app
      
 option = st.selectbox("Which fairytale you want to summarize?", 
                       ("Little Snow White", "The Ugly Duckling", "Aladdin and the Wonderful Lamp", "The Sleeping Beauty", "Puss-in-Boots", "Adventures of Tom Thumb", "The Three Bears", "The Little Match Girl", "Beauty and the Beast", "The Story of Cinderella", "Jack the Giant Killer", "Jack and the Beanstalk", "Dick Whittington and His Cat", "The Story of Bluebeard", "Little Red Riding-Hood", "Sindbad the Sailor", "Hansel and Gretel", "The Goose Girl"))
